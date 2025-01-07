6e3657a25feebc1b8b988c4877e1a902d0be96ca
theepicsaxguy <benjamin.sanden@hotmail.com>
2025-01-07 14:35:20 +0100

    fix

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
 import '../../index.css';
-import App from '../../App';
-import { ThemeProvider } from 'styled-components';
-
-import theme from '../../styles/theme';
-import GlobalStyle from '../../styles/theme/globalStyle';
 import { ContainerFluid } from '../../components/container';
 import BannerSection from './components/banner-section';
 import CarouselSection from './components/carousel-section';
@@ -20,9 +15,6 @@ const Home = () => {
             <Footer />
         </ContainerFluid>
     );
-}
+};
 
 export default Home;
-
-
-
